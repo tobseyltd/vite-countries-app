c20f3b3014d965956f3809c9bc2f90465690353d
tobseyltd <mail@tobsey.limited>
2023-04-13 15:20:52 +0200

    Created Header Component & Dark Mode

---
+++ src/components/Header.tsx
@@ -0,0 +1,29 @@
+import { Flex, Box, Text, Stack, Switch, useColorMode } from "@chakra-ui/react";
+
+const Header = () => {
+  const { colorMode, toggleColorMode } = useColorMode();
+  return (
+    <>
+      <Flex justify={"space-between"} align={"center"} px={10} py={7}>
+        <Box>
+          <Text as="b" fontSize={"2xl"}>
+            Where in the World?
+          </Text>
+        </Box>
+        <Stack align={"center"} direction={"row"}>
+          <Switch
+            colorScheme="green"
+            isChecked={colorMode === "dark"}
+            onChange={toggleColorMode}
+            size={"md"}
+          />
+          <Text as="b" fontSize={"sm"}>
+            Dark Mode
+          </Text>
+        </Stack>
+      </Flex>
+    </>
+  );
+};
+
+export default Header;
